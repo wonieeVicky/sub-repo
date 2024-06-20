9a2ef2a98ebec6ff8203743933a44eec1d08ee48
wonieeVicky <hwfongfing@gmail.com>
2024-06-20 17:08:28 +0900

    test: push

---
+++ src/components/Button2.tsx
@@ -0,0 +1,19 @@
+﻿import React from 'react';
+
+const Button = ({ text, onClick, ...props }: any) => {
+  return (
+    <button
+      style={{
+        border: '1px solid blue',
+        padding: '15px 20px',
+        borderRadius: 8,
+        ...props.style
+      }}
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
+};
+
+export default Button;
